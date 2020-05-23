1206cf01c2358d9edf6695be0dcd040cd2030054
anjali0604 <anjalisharma060400@gmail.com>
2020-05-23 23:43:07 +0530

    Assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,65 +1,70 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class DishDetail extends Component {
-
-    renderDish(dish) {
-        if (dish!=null){
-            return(
-                <Card className="col-12 col-md-5 m-1">
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle><strong>{dish.name}</strong></CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>  
-            );
-        } else {
-            return(
-                <div></div>
-            );
-        }
+function RenderDish({dish}) {
+    if (dish!=null){
+        return(
+            <Card className="col-12 col-md-5 m-1">
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle><strong>{dish.name}</strong></CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>  
+        );
+    } else {
+        return(
+            <div></div>
+        );
     }
+}
 
-    renderComments(array) {
-        if(array.length !== 0) {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {array.map(comment => (
-                        <ul className="list-unstyled">
-                            <li>
+function RenderComments({comments}) {
+    if(comments != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return(
+                            <li key={comment.id}>
                                 <p>{comment.comment}</p>
                                 <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                             </li>
-                        </ul>
-                    )
-                    )}
-                </div>);
-        } else {
-            return(
-                <div></div>
-            );
-        }
+                        );
+                    })}
+                </ul>
+            </div>);
+    } else {
+        return(
+            <div></div>
+        );
     }
+}
 
-    render() {
-        let dish;
-        if (this.props.selectedDish) {
-            dish = (
-                <div className="row">
-                    {this.renderDish(this.props.selectedDish)}
-                    {this.renderComments(this.props.selectedDish.comments)}
-                </div>
-            )
-        } else {
-            dish = <div></div>
-        }
+const DishDetail = (props) => {
+    if (props.dish!=null) {
         return(
             <div className="container">
-                {dish}
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
         );
+    } else {
+        return <div></div>
     }
 }
 
